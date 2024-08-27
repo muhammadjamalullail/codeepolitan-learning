2cd10bd582d85dc63815a6a5a0f38de7e130fa4b
Muhammad Jamalullail <muhammadjamalullail30@gmail.com>
2024-08-27 05:38:25 -0700

    mod: mengganti semua file pakai vite

--- javascript-async/main.js
+++ javascript-async/main.js
@@ -1,13 +1,60 @@
-const container = document.querySelector("div");
+// Callback Version
+const requestCallback = (url, success, failure) => {
+  const delay = Math.floor(Math.random() * 4500) + 500;
+  setTimeout(() => {
+    if (delay > 4000) {
+      failure("Error: Connection Timeout");
+    } else {
+      success(`Success: ${url} (${delay}ms)`);
+    }
+  }, delay);
+};
 
-const element = document.createElement("h1");
-const textNode = document.createTextNode("Muhammad Jamalullail");
-element.appendChild(textNode);
+// Promise Version
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 5000) {
+        reject("Error: Connection Timeout");
+      } else {
+        resolve(`Success: ${url} (${delay}ms)`);
+      }
+    }, delay);
+  });
+};
 
-function add() {
-  const element = document.createElement("h2");
-  element.append("Muhammad Nazhan");
-  return element;
-}
+// requestCallback(
+//   "moview.com",
+//   function (response) {
+//     console.log(`${response}`);
+//   },
+//   function (error) {
+//     console.log(`${error}`);
+//   }
+// );
 
-
+requestPromise("instagram.com").then(
+  (response) => {
+    console.log(response);
+    requestPromise("instagram.com").then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    requestPromise("instagram.com").then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  (error) => {
+    console.log(error);
+  }
+);
